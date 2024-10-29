390bc9045979ad8d0dd7322fdff3caea0612ebee
Vicente Figueiredo <sawsent@github.com>
2024-10-29 17:50:07 +0000

    added simple POST request, TODO-style following documentation

--- src/main/scala/com/akkamelo/api/endpoint/HelloWorld.scala
+++ src/main/scala/com/akkamelo/api/endpoint/HelloWorld.scala
@@ -1,18 +1,33 @@
 package com.akkamelo.api.endpoint
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 
 object HelloWorld extends App {
   implicit val system = ActorSystem("hello-world")
   implicit val executionContext = system.dispatcher
 
-  val route =
-    path("hello") {
-      get {
-        complete("Hello, World!")
+  var messages = List.empty[String]
+
+  val route: Route = {
+    concat(
+      path("messages") {
+        get {
+          complete(messages.toString())
+        }
+      },
+      post {
+        path("messages") {
+          entity(as[String]) { message =>
+            messages = messages :+ message
+            complete(s"Message '$message' has been stored.")
+          }
+        }
       }
-    }
+    )
+
+  }
 
   val bindingFuture = akka.http.scaladsl.Http().newServerAt("localhost", 8080).bind(route)
 
