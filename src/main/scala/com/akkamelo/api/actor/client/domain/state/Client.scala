9cac2c0f201fabda9be339b6003e4e8a8a153c39
Vicente Figueiredo <sawsent@github.com>
2024-11-04 18:07:01 +0000

    changed slf4j logging formatting to match akka logging pattern. Added optimization to client by only keeping at most 100 transactions in memory

--- src/main/scala/com/akkamelo/api/actor/client/domain/state/Client.scala
+++ src/main/scala/com/akkamelo/api/actor/client/domain/state/Client.scala
@@ -9,22 +9,33 @@ object Client {
   val initial: Client = Client(0, List.empty[Transaction], 0)
 }
 
-case class Client(id: Int, transactions: List[Transaction], limit: Int) {
+case class Client(id: Int, transactions: List[Transaction], limit: Int, balanceSnapshot: Int = 0) {
   def add(transaction: Transaction): Client = {
     transaction match {
       case c: Credit =>
       case d: Debit => if (balance - d.value < -1 * limit) throw InvalidTransactionException("Can't debit to lower than limit")
     }
-    this.copy(id, transaction +: transactions, limit)
+    if (transactions.length >= 100) {
+      val currentBalance = balance(transactions.dropRight(10))
+      copy(transactions = transaction +: transactions.take(10), balanceSnapshot = currentBalance)
+    } else {
+      copy(transactions = transaction +: transactions)
+    }
+    // this.copy(id, transaction +: transactions, limit)
   }
 
   def getStatement: Statement = {
     val balanceInformation = BalanceInformation(balance, limit, LocalDateTime.now())
+    print(transactions.length)
     val lastTransactions = transactions.slice(0, 10)
     Statement(balanceInformation, lastTransactions)
   }
 
   def balance: Int = {
+    balance(transactions)
+  }
+
+  def balance(transactions: List[Transaction]): Int = balanceSnapshot + {
     transactions.foldRight(0)((t: Transaction, acc: Int) => t match {
       case Credit(value, _, _) => acc + value
       case Debit(value, _, _) => acc - value
