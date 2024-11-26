4583f5d3ae4bf0785264eab65e6e3ce489e9bd32
Vicente Figueiredo <sawsent@github.com>
2024-11-26 17:30:18 +0000

    made not implemented tests fail.

--- src/test/scala/com/akkamelo/api/actor/client/ClientActorSpec.scala
+++ src/test/scala/com/akkamelo/api/actor/client/ClientActorSpec.scala
@@ -6,23 +6,23 @@ import com.akkamelo.api.actor.common.BaseActorSpec
 class ClientActorSpec extends BaseActorSpec(ActorSystem("ClientActorSpec")) {
 
   "A client Actor" should "reply with the balance and limit after adding a transaction" in {
-
+    throw new NotImplementedError("Not implemented")
   }
 
   it should "reply with the statement when requested" in {
-
+    throw new NotImplementedError("Not implemented")
   }
 
   it should "reply with an ActorProcessingFailure when the Transaction doesn't go through and ClientState should stay the same" in {
-
+    throw new NotImplementedError("Not implemented")
   }
 
   it should "reply with an ActorNotFound when the ClientState doesn't exist" in {
-
+    throw new NotImplementedError("Not implemented")
   }
 
   it should "reply with an ActorAlreadyExists when the ClientState already exists" in {
-
+    throw new NotImplementedError("Not implemented")
   }
 
 }
